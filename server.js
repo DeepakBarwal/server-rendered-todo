7de26f07d00c4329632884b530a2464f603d7f11
DeepakBarwal <deepakbarwal98@gmail.com>
2021-01-11 20:06:10 +0530

    cleanup
    Signed-off-by: DeepakBarwal <deepakbarwal98@gmail.com>

--- server.js
+++ server.js
@@ -3,24 +3,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
 
 let tasks = [];
 
 app.get('/', (req, res) => {
-  let taskList = tasks.map((task) => `<li>${task}</li>`).join('\n');
-  res.send(`
-        <html>
-        <body>
-            <form action="/" method="POST">
-                <input name="newTask">
-                <button type="submit">ADD</button>
-                <ul>
-                    ${taskList}
-                </ul>
-            </form>
-        </body>
-        </html>
-    `);
+  // let taskList = tasks.map((task) => `<li>${task}</li>`).join('\n');
+  res.render('home', {
+    title: 'Todo List',
+    tasks,
+  });
 });
 
 app.post('/', (req, res) => {
